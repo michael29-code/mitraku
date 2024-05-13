a5310db0c64c5f1c144a4c8de8ecb49cbb31140f
Richie <richiehartonosan@gmail.com>
2024-05-13 09:25:29 +0700

    A

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1110,6 +1110,7 @@
                 </main>
 
                 <footer class="py-16 text-center text-sm text-black dark:text-white/70">
+                    HIIIIIIIIIIIIIIIIIII
                     Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                 </footer>
             </div>
