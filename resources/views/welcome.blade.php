29c93ff20920d3a2f6297cc4b13e7c0638a9d38a
N3K0Y4 <nekoya364@gmail.com>
2024-05-13 15:09:14 +0700

    cek

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1110,7 +1110,7 @@
                 </main>
 
                 <footer class="py-16 text-center text-sm text-black dark:text-white/70">
-                    Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                    PLaravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                 </footer>
             </div>
         </div>
