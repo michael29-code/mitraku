363144de25abf6c2d589286a17ebf6ca9dd4f38d
N3K0Y4 <nekoya364@gmail.com>
2024-05-20 16:44:30 +0700

    Peter

--- resources/views/HomePage.blade.php
+++ resources/views/HomePage.blade.php
@@ -7,7 +7,7 @@
     </div>
 
     <div class="container text-center body-large-bold">
-            <p>Whether it's selling your current home, getting financing, or buying a new home, we make it easy and efficient. The best part? you'll save a bunch of money and time with our services.</p>
+            Whether it's selling your current home, getting financing, or buying a new home, we make it easy and efficient. The best part? you'll save a bunch of money and time with our services.
     </div>
 
     <div class="container text-center">
@@ -17,4 +17,141 @@
     <div class="container text-center">
             <img src="/images/RumahNekoya.png" class="img-fluid" alt="rumah-nekoya">
     </div>
+
+    <div class="container text-center">
+        <h2>Trusted by over a lot of companies.</h2>
+
+        <div class="container text-center row row-cols-5">
+            <div class="col">Sponsor</div>
+            <div class="col">Sponsor</div>
+            <div class="col">Sponsor</div>
+            <div class="col">Sponsor</div>
+            <div class="col">Sponsor</div>
+            <div class="col">Sponsor</div>
+            <div class="col">Sponsor</div>
+            <div class="col">Sponsor</div>
+            <div class="col">Sponsor</div>
+        </div>
+    </div>
+
+    <div class="row row-cols-2">
+        <img src="/images/Dinding.png"  alt="dinding">
+
+        <div class="col">
+            <h2>Multi Curency Increase Update</h2>
+
+            <div class="body-medium-bold">
+                Whether it's selling your current home, getting financing, or buying a new home, we make it easy and efficient. The best part? you'll save a bunch of money and time with our services.
+            </div>
+        </div>
+    </div>
+
+    <div class="container text-center">
+        <h2>Benefit and Regulated</h2>
+    </div>
+
+    <div class="container text-center row row-cols-4">
+        <div class="container text-center">
+            <img src="" alt="">
+            <h3>Property Insurance</h3>
+            <div class="body-medium-bold">
+                We offer our customer property protection of liability coverage and insurance for their better life.
+            </div>
+        </div>
+        <div class="container text-center">
+            <img src="" alt="">
+            <h3>Best Price</h3>
+            <div class="body-medium-bold">
+                We offer our customer property protection of liability coverage and insurance for their better life.
+            </div>
+        </div>
+        <div class="container text-center">
+            <img src="" alt="">
+            <h3>Property Insurance</h3>
+            <div class="body-medium-bold">
+                We offer our customer property protection of liability coverage and insurance for their better life.
+            </div>
+        </div>
+        <div class="container text-center">
+            <img src="" alt="">
+            <h3>Best Price</h3>
+            <div class="body-medium-bold">
+                We offer our customer property protection of liability coverage and insurance for their better life.
+            </div>
+        </div>
+        <div class="container text-center">
+            <img src="" alt="">
+            <h3>Lowest Commision</h3>
+            <div class="body-medium-bold">
+                We offer our customer property protection of liability coverage and insurance for their better life.
+            </div>
+        </div>
+        <div class="container text-center">
+            <img src="" alt="">
+            <h3>Overall Control</h3>
+            <div class="body-medium-bold">
+                We offer our customer property protection of liability coverage and insurance for their better life.
+            </div>
+        </div>
+        <div class="container text-center">
+            <img src="" alt="">
+            <h3>Lowest Commision</h3>
+            <div class="body-medium-bold">
+                We offer our customer property protection of liability coverage and insurance for their better life.
+            </div>
+        </div>
+        <div class="container text-center">
+            <img src="" alt="">
+            <h3>Overall Control</h3>
+            <div class="body-medium-bold">
+                We offer our customer property protection of liability coverage and insurance for their better life.
+            </div>
+        </div>
+    </div>
+
+    <div class="row row-cols-2">
+        <div class="col">
+            <h2>Multi Curency Increase Update</h2>
+
+            <div class="body-medium-bold">
+                Whether it's selling your current home, getting financing, or buying a new home, we make it easy and efficient. The best part? you'll save a bunch of money and time with our services.
+            </div>
+        </div>
+
+        <img src="/images/Dinding.png"  alt="dinding">
+
+    </div>
+
+    <div class="row row-cols-2">
+        <div class="col">
+            <h2>Multi Curency Increase Update</h2>
+
+            <div class="body-medium-bold">
+                Whether it's selling your current home, getting financing, or buying a new home, we make it easy and efficient. The best part? you'll save a bunch of money and time with our services.
+            </div>
+
+            <div class="row row-cols-3">
+                <div class="container text-center">
+                    <img src="" alt="">
+                    <h3>7.4%</h3>
+                    <div class="body-medium-bold">Property Return Rate</div>
+                </div>
+                <div class="container text-center">
+                    <img src="" alt="">
+                    <h3>3.586</h3>
+                    <div class="body-medium-bold">Property in Sell & Rent</div>
+                </div>
+                <div class="container text-center">
+                    <img src="" alt="">
+                    <h3>2.540</h3>
+                    <div class="body-medium-bold">Daily Completed Transactions</div>
+                </div>
+            </div>
+        </div>
+
+        <img src="/images/DenahRumah.png"  alt="denah-rumah">
+
+    </div>
+    
+
 @endsection
