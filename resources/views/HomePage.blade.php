021a713749b131c59e61250ad6e9376711a8d2c4
N3K0Y4 <nekoya364@gmail.com>
2024-05-27 22:34:33 +0700

    peter
    homepage

--- resources/views/HomePage.blade.php
+++ resources/views/HomePage.blade.php
@@ -20,7 +20,7 @@
 
     <div class="border m-5 row justify-content-center text-center">        
         <h2>Trusted by over a lot of companies.</h2>
-        <div class="justify-content-center text-center row row-cols-5">
+        <div class="justify-content-center text-center row row-cols-5" style="">
             <div class="border rounded-3 m-2 p-3 col" style="width: 13rem;">Sponsor</div>
             <div class="border rounded-3 m-2 p-3 col" style="width: 13rem;">Sponsor</div>
             <div class="border rounded-3 m-2 p-3 col" style="width: 13rem;">Sponsor</div>
@@ -33,122 +33,107 @@
         </div>
     </div>
 
-<div class="row row-cols-2">
-<img src="/images/Dinding.png"  alt="dinding">
-
-<div class="col body-medium-bold">
-    <h2>Multi Curency Increase Update</h2>
-
-        Whether it's selling your current home, getting financing, or buying a new home, we make it easy and efficient. The best part? you'll save a bunch of money and time with our services.
-
-    <button type="button" class="btn btn-primary btn-sm">Button</button>
-
-</div>
-</div>
-
-<div class="text-center">
-<h2>Benefit and Regulated</h2>
-</div>
-
-<div class="text-center row row-cols-4">
-<div class="text-center">
-    <h3>Property Insurance</h3>
-    <div class="body-medium-bold">
-        We offer our customer property protection of liability coverage and insurance for their better life.
-    </div>
-</div>
-<div class="text-center">
-    <h3>Best Price</h3>
-    <div class="body-medium-bold">
-        We offer our customer property protection of liability coverage and insurance for their better life.
-    </div>
-</div>
-<div class="text-center">
-    <img src="" alt="">
-    <h3>Property Insurance</h3>
-    <div class="body-medium-bold">
-        We offer our customer property protection of liability coverage and insurance for their better life.
-    </div>
-</div>
-<div class="text-center">
-    <img src="" alt="bill-icon">
-    <h3>Best Price</h3>
-    <div class="body-medium-bold">
-        We offer our customer property protection of liability coverage and insurance for their better life.
-    </div>
-</div>
-<div class="text-center">
-    <img src="" alt="">
-    <h3>Lowest Commision</h3>
-    <div class="body-medium-bold">
-        We offer our customer property protection of liability coverage and insurance for their better life.
-    </div>
-</div>
-<div class="text-center">
-    <img src="" alt="">
-    <h3>Overall Control</h3>
-    <div class="body-medium-bold">
-        We offer our customer property protection of liability coverage and insurance for their better life.
-    </div>
-</div>
-<div class="text-center">
-    <img src="" alt="">
-    <h3>Lowest Commision</h3>
-    <div class="body-medium-bold">
-        We offer our customer property protection of liability coverage and insurance for their better life.
-    </div>
-</div>
-<div class="text-center">
-    <img src="" alt="">
-    <h3>Overall Control</h3>
-    <div class="body-medium-bold">
-        We offer our customer property protection of liability coverage and insurance for their better life.
-    </div>
-</div>
-</div>
-
-<div class="row row-cols-2">
-<div class="col">
-    <h2>Multi Curency Increase Update</h2>
-
-    <div class="body-medium-bold">
-        Whether it's selling your current home, getting financing, or buying a new home, we make it easy and efficient. The best part? you'll save a bunch of money and time with our services.
-    </div>
-</div>
-
-<img src="/images/Dinding.png"  alt="dinding">
-
-</div>
-
-<div class="row row-cols-2">
-<div class="col">
-    <h2>Multi Curency Increase Update</h2>
-
-    <div class="body-medium-bold">
-        Whether it's selling your current home, getting financing, or buying a new home, we make it easy and efficient. The best part? you'll save a bunch of money and time with our services.
+    <div class="text-center row row-cols-2 m-3">
+        <img src="/images/Dinding.png"  alt="dinding">
+        <div class="align-self-center col body-medium-bold">
+            <h2>Multi Curency Increase Update</h2>
+            <p>Whether it's selling your current home, getting financing, or buying a new home, we make it easy and efficient. The best part? you'll save a bunch of money and time with our services.</p>
+            <button type="button" class="btn btn-primary btn-sm">Button</button>
+        </div>
     </div>
 
-    <div class="row row-cols-3">
+    <div class="m-5 mx-auto m-3 text-center" style="width: 50rem;">
+        <h2>Benefit and Regulated</h2>
+    </div>
+
+    <div class="justify-content-center text-center row row-cols-4 m-3">
         <div class="text-center">
-            <img src="" alt="">
-            <h3>7.4%</h3>
-            <div class="body-medium-bold">Property Return Rate</div>
+            <h3>Property Insurance</h3>
+            <div class="body-medium-bold">
+                We offer our customer property protection of liability coverage and insurance for their better life.
+            </div>
         </div>
         <div class="text-center">
-            <img src="" alt="">
-            <h3>3.586</h3>
-            <div class="body-medium-bold">Property in Sell & Rent</div>
+            <h3>Best Price</h3>
+            <div class="body-medium-bold">
+                We offer our customer property protection of liability coverage and insurance for their better life.
+            </div>
         </div>
         <div class="text-center">
-            <img src="" alt="">
-            <h3>2.540</h3>
-            <div class="body-medium-bold">Daily Completed Transactions</div>
+            <h3>Property Insurance</h3>
+            <div class="body-medium-bold">
+                We offer our customer property protection of liability coverage and insurance for their better life.
+            </div>
+        </div>
+        <div class="text-center">
+            <h3>Best Price</h3>
+            <div class="body-medium-bold">
+                We offer our customer property protection of liability coverage and insurance for their better life.
+            </div>
+        </div>
+        <div class="text-center">
+            <h3>Lowest Commision</h3>
+            <div class="body-medium-bold">
+                We offer our customer property protection of liability coverage and insurance for their better life.
+            </div>
+        </div>
+        <div class="text-center">
+            <h3>Overall Control</h3>
+            <div class="body-medium-bold">
+                We offer our customer property protection of liability coverage and insurance for their better life.
+            </div>
+        </div>
+        <div class="text-center">
+            <h3>Lowest Commision</h3>
+            <div class="body-medium-bold">
+                We offer our customer property protection of liability coverage and insurance for their better life.
+            </div>
+        </div>
+        <div class="text-center">
+            <h3>Overall Control</h3>
+            <div class="body-medium-bold">
+                We offer our customer property protection of liability coverage and insurance for their better life.
+            </div>
         </div>
     </div>
-</div>
 
-<img src="/images/DenahRumah.png"  alt="denah-rumah">
+    <div class="text-center row row-cols-2 m-3">
+        <div class="align-self-center col body-medium-bold">
+            <h2>Multi Curency Increase Update</h2>
+            <p>Whether it's selling your current home, getting financing, or buying a new home, we make it easy and efficient. The best part? you'll save a bunch of money and time with our services.</p>
+            <button type="button" class="btn btn-primary btn-sm">Button</button>
+        </div>
+        <img src="/images/Dinding.png"  alt="dinding">
+    </div>
 
-</div>
+    <div class="text-center row row-cols-2 m-3">
+        <div class="col align-self-center">
+            <h2>Multi Curency Increase Update</h2>
+            <div class="body-medium-bold align-self-center">
+                Whether it's selling your current home, getting financing, or buying a new home, we make it easy and efficient. The best part? you'll save a bunch of money and time with our services.
+            </div>
+            <div class="text-center row row-cols-3 m-3">
+                <div class="text-center">
+                    <h3>7.4%</h3>
+                    <div class="body-medium-bold">
+                        Property Return Rate
+                    </div>
+                </div>
+                <div class="text-center">
+                    <h3>3.586</h3>
+                    <div class="body-medium-bold">
+                        Property in Sell & Rent
+                    </div>
+                </div>
+                <div class="text-center">
+                    <h3>2.540</h3>
+                    <div class="body-medium-bold">
+                        Daily Completed Transactions
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img src="/images/DenahRumah.png"  alt="denah-rumah">
+    </div>
 
 @endsection
