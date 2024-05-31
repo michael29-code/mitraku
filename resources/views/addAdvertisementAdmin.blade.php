4d23e5cb4f8843a43d9858099ba003cb461c6d5d
N3K0Y4 <nekoya364@gmail.com>
2024-05-31 14:39:52 +0700

    peter
    manage, create, update advertise

--- resources/views/addAdvertisementAdmin.blade.php
+++ resources/views/addAdvertisementAdmin.blade.php
@@ -1,5 +1,35 @@
 @extends('layouts.layoutAdmin')
 
 @section('content')
+    
+    <div class="row m-3" style="height: 3rem">
+        <button type="button" class="btn btn-dark" style="width: 7rem; height: 3rem">Dark</button>
+        <h2 class="col" style="width: 10rem; height 3rem">Create Advertisement</h2>
+    </div>
+    
+    <form>
+        <div class="mb-3">
+            <label for="exampleInputTitle" class="form-label">Title</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPrice" class="form-label">Price</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3 form-check">
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Subscription Duration</option>
+                <option value="1">Daily</option>
+                <option value="2">Monthly</option>
+                <option value="3">Annualy</option>
+            </select>
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+
+        <button type="button" class="btn btn-primary">Submit</button>
+    </form>
 
 @endsection
--- resources/views/viewAdvertisementAdmin.blade.php
+++ resources/views/viewAdvertisementAdmin.blade.php
@@ -2,4 +2,73 @@
 
 @section('content')
 
+    <div class="shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+        <div class="row justify-content-center p-3">
+            <div class="row col" style="height: 3rem">
+                <button type="button" class="btn btn-dark" style="width: 7rem; height: 3rem">Dark</button>
+                <h2 class="col" style="width: 10rem; height 3rem">View Advertisement</h2>
+            </div>
+            <form class="d-flex col" role="search" style="width: 20rem; height: 3rem">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            </form>
+        </div>
+        <div class="row row-cols-2 p-3 justify-content-center">
+            <div class="row col shadow p-2 m-3 bg-body-tertiary rounded" style="width: 37rem; height:13rem">
+                <div class="col">
+                    <h5>price</h5>
+                    <h4>title</h4>
+                    <p>isi</p>
+                    <button type="button" class="btn btn-danger">Delete</button>
+                </div>
+                <div class="col bg-info"></div>
+            </div>
+            <div class="row col shadow p-2 m-3 bg-body-tertiary rounded" style="width: 37rem; height:13rem">
+                <div class="col">
+                    <h5>price</h5>
+                    <h4>title</h4>
+                    <p>isi</p>
+                    <button type="button" class="btn btn-danger">Delete</button>
+                </div>
+                <div class="col bg-info"></div>
+            </div>
+            <div class="row col shadow p-2 m-3 bg-body-tertiary rounded" style="width: 37rem; height:13rem">
+                <div class="col">
+                    <h5>price</h5>
+                    <h4>title</h4>
+                    <p>isi</p>
+                    <button type="button" class="btn btn-danger">Delete</button>
+                </div>
+                <div class="col bg-info"></div>
+            </div>
+            <div class="row col shadow p-2 m-3 bg-body-tertiary rounded" style="width: 37rem; height:13rem">
+                <div class="col">
+                    <h5>price</h5>
+                    <h4>title</h4>
+                    <p>isi</p>
+                    <button type="button" class="btn btn-danger">Delete</button>
+                </div>
+                <div class="col bg-info"></div>
+            </div>
+            <div class="row col shadow p-2 m-3 bg-body-tertiary rounded" style="width: 37rem; height:13rem">
+                <div class="col">
+                    <h5>price</h5>
+                    <h4>title</h4>
+                    <p>isi</p>
+                    <button type="button" class="btn btn-danger">Delete</button>
+                </div>
+                <div class="col bg-info"></div>
+            </div>
+            <div class="row col shadow p-2 m-3 bg-body-tertiary rounded" style="width: 37rem; height:13rem">
+                <div class="col">
+                    <h5>price</h5>
+                    <h4>title</h4>
+                    <p>isi</p>
+                    <button type="button" class="btn btn-danger">Delete</button>
+                </div>
+                <div class="col bg-info"></div>
+            </div>
+        </div>
+    </div>
+
 @endsection
