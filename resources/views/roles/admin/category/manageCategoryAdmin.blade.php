7e9bcdc2a9a30dc3809e4ab16c074d4b51366e69
CarolineNatalia <carolinenataliamran97@gmail.com>
2024-07-09 13:41:51 +0700

    benerin error kategori admin

--- resources/views/roles/admin/category/manageCategoryAdmin.blade.php
+++ resources/views/roles/admin/category/manageCategoryAdmin.blade.php
@@ -18,16 +18,10 @@
                         </div>
                         <div class="row">
                             <div class="col mx-2 my-2 py-2 border-bottom d-flex align-items-center">
-                                <a href="{{ route('add-category') }}">Add Category</a>
+                                <i class="bi bi-plus-square-fill"></i>
+                                <a href="{{ route('add-category') }}" class="text-decoration-none text-black mx-4" style="font-size: 20px;" >Add Category</a>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col mx-5 my-2 py-2 ">
-                                <i class="fa-solid fa-pen-to-square fs-4 me-3"></i>
-                                <a href="{{ route('update-category') }}" class="text-decoration-none text-black" style="font-size: 20px;">Update Category</a>
-                            </div>
-                        </div>
-                        
                     </div>
                 </div>
             </div>
