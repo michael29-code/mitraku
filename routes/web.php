c41f9f1874c915765acea0d10e186e6fd85b9ceb
CarolineNatalia <carolinenataliamran97@gmail.com>
2024-07-09 13:22:13 +0700

    benerin web.php lagi
    barusan banget benerin setelah pull mekel

--- routes/web.php
+++ routes/web.php
@@ -55,12 +55,31 @@ Route::group(['middleware' => ['CheckUser:1']], function () {
     Route::get('/update-category/{jenisKategori}', [CategoryController::class, 'updateCategoryAdmin'])->name('update-category');
     Route::delete('/view-category/{jenisKategori}', [CategoryController::class, 'destroy'])->name('destroy-category');
     Route::put('/update-category/{jenisKategori}', [CategoryController::class, 'update'])->name('update-category.submit');
+
+    //ADVERTISMENT
+    Route::get('/manage-advertisement', function () {
+        return view('roles.admin.advertise.manageAdvertisementAdmin');
+    });
+    Route::get('/manage-advertisement-view', [AdvertisementController::class, ''])->name('view_advertisement');
+    Route::get('/manage-advertisement-add', [AdvertisementController::class, ''])->name('add_advertisement');
+    Route::get('/manage-advertisement-delete', [AdvertisementController::class, ''])->name('delete_advertisement');
+    Route::get('/manage-advertisement-update', [AdvertisementController::class, ''])->name('update_advertisement');
+
+    //VIEW USER
+    Route::get('/viewUser', function () {
+        return view('roles.admin.block.viewUser');
+    });
 });
 
 // UNTUK USER
 Route::group(['middleware' => ['CheckUser:2']], function () {
-    // CREATE MITRA
+    //MITRA
     Route::get('/view-mitra', [MitraController::class, 'mitra'])->name('view-mitra');
+    Route::get('detailMitra', function () {
+        return view('roles.user.mitra.detailMitra');
+    })->name('detailMitra');
+    
+    // CREATE MITRA
     Route::get('/create-mitra-step-1', [MitraController::class, 'createMitra1']);
     Route::get('/create-mitra-step-2', [MitraController::class, 'createMitra2']);
     Route::get('/create-mitra-step-3', [MitraController::class, 'createMitra3']);
@@ -73,97 +92,23 @@ Route::group(['middleware' => ['CheckUser:2']], function () {
     Route::get('/profile', function () {
         return view('profile');
     });
+
+    //ADVERTISMENT
+    Route::get('/userAdvertisement', function () {
+        return view('roles.user.advertise.userAdvertisement');
+    })->name('view-advertisement');
+
+
+    // FORM PENGAJUAN
+    Route::get('/form-pengajuan', function () {
+        return view('roles.user.mitra.viewFormPengajuan');
+    })->name('form-pengajuan');
+
+
+    //PAYMENT
+    Route::get('/payment', function () {
+        return view('paymentPage');
+    });    
 });
 
-
-// PUNYA NIAAAAAAAAAAA
-
-
-Route::get('/profile/{id}', [UserController::class,'show']);
-
-// Route::get('/create-mitra', function () {
-//     return view('createMitra');
-// });
-
-
-Route::get('/add-detail-mitra', [MitraController::class, 'addDetailMitra']);
-Route::get('/add-detailing-mitra', [MitraController::class, 'addDetailingMitra']);
-
-Route::get('/mitra', function () {
-    return view('roles.user.mitra.mitra');
-});
-
-
-Route::get('/manage-blog',[BlogController::class,'manageBlog']);
-Route::get('/view-blog', [BlogController::class,'view']);
-Route::get('/write-blog', [BlogController::class,'writeBlog']);
-Route::post('/store-blog', [BlogController::class,'store']);
-Route::get('/edit-blog/{blog:slug}',[BlogController::class,'edit']);
-Route::put('/update-blog/{blog:slug}',[BlogController::class,'update']);
-Route::delete('/delete-blog/{blog:slug}',[BlogController::class,'destroy']);
-Route::get('/view-blog-detail/{blog:slug}', [BlogController::class,'show']);
-Route::get('/search', [BlogController::class,'search'])->name('search');
-
-
-Route::get('/manage-category', function () {
-    return view('roles.admin.category.manageCategoryAdmin');
-})->name('manage-category');
-
-Route::get('/view-category', [CategoryController::class, 'viewCategoryAdmin'])->name('view-category');
-Route::get('/add-category', [CategoryController::class, 'addCategoryAdmin'])->name('add-category');
-Route::post('/store-category', [CategoryController::class, 'store'])->name('store-category');
-Route::get('/delete-category', [CategoryController::class, 'deleteCategoryAdmin'])->name('delete-category');
-Route::get('/update-category/{jenisKategori}', [CategoryController::class, 'updateCategoryAdmin'])->name('update-category');
-Route::delete('/view-category/{jenisKategori}', [CategoryController::class, 'destroy'])->name('category.destroy');
-
-
-Route::get('/manage-advertisement', function () {
-    return view('roles.admin.advertise.manageAdvertisementAdmin');
-})->name('manage-advertisement');
-Route::get('/manage-advertisement-view', [AdvertisementController::class, 'index'])->name('view-advertisement-admin');
-Route::get('/manage-advertisement-add', [AdvertisementController::class, 'create'])->name('add-advertisement');
-Route::post('/manage-advertisement-store', [AdvertisementController::class, 'store'])->name('store-advertisement');
-Route::delete('/advertisements/{id}', [AdvertisementController::class, 'destroy'])->name('delete-advertisement');
-Route::get('/advertisements/{id}/edit', [AdvertisementController::class, 'edit'])->name('edit-advertisement');
-Route::put('/advertisements/{id}', [AdvertisementController::class, 'update'])->name('update-advertisement');
-// PUNYA NIAAAAAAAAAAA END
-
-
-
-Route::get('/viewUser', function () {
-    return view('roles.admin.block.viewUser');
-});
-
-Route::get('/userAdvertisement', function () {
-    return view('roles.user.advertise.userAdvertisement');
-})->name('view-advertisement');
-
-Route::get('/view-pengajuan', [PengajuanController::class, 'view']);
-Route::get('/form-pengajuan', [PengajuanController::class, 'create']);
-Route::post('/add-pengajuan', [PengajuanController::class, 'store']);
-// Route::get('/pengajuan/pdf/{id}', [PengajuanController::class, 'download']);
-Route::get('/download/{pengajuan}', DownloadPDFController::class);
-
-Route::get('/pengajuan/edit/{id}', [PengajuanController::class, 'edit']);
-Route::put('/pengajuan/update/{id}', [PengajuanController::class, 'update']);
-Route::delete('/pengajuan/delete/{id}',[PengajuanController::class,'destroy']);
-
-Route::get('create-mitra', [MitraController::class, 'create']);
-Route::post('viewMitra', [MitraController::class, 'store']);  #ini kayaknya harus di benerin pathnyaa, ambigu
-
-
-Route::get('/payment', function () {
-    return view('paymentPage');
-});
-
-Route::get('/createMitra', function () {
-    return view('roles.user.create_mitra.createMitra1');
-});
-
-Route::get('detailMitra', function () {
-    return view('roles.user.mitra.detailMitra');
-})->name('detailMitra');
-
-// Route::get('mitraaa', function () {
-//     return view('roles.user.mitra.mitra');
-// });
+Route::get('/profile/{id}', [UserController::class, 'show']);
